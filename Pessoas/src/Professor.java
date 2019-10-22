0b24748bda900e8dbb70e53e72949214b1ae4ec2
ALLAN HENRIQUES TEIXEIRA <allanhenriquesteixeira@ice.ufjf.br>
2019-10-22 17:08:47 -0300

    IMplementa Professor

--- Pessoas/src/Professor.java
+++ Pessoas/src/Professor.java
@@ -1,2 +1,43 @@
-public class Professor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Pessoa{
+
+   private String departamento;
+   private List <Materia> listaMateria = new Arraylist<>();
+   private int totalHoras;
+   private String departamento;
+
+   // construtor
+   public Professor(String nome, String CPF, String matricula, List<Materia> listaMateria, String departamento){
+       super( Professor.nome, Professor.CPF, Professor.matricula, Professor.listaMateria);
+       this.departamento = departamento;
+       }
+   
+   public List<Materia> getListaMateria(){
+
+     return listaMateria;
+   }
+   public void addMateria(Materia materia){
+      
+      listaMateria.add(materia);
+   }  
+   public void removeMateria(Materia materia){
+    // método para remover uma matéria
+     listaMateria.remove(materia);
+   }
+   public String getDepartamento(){
+      return departamento;
+   }
+
+   public void calculaTotalHoras(){
+    // método para calcular o total de horas, somando toda a carga       //horária das matérias lecionada  mais o horário de atendimento
+    int totalHoras = 0;
+        for (Materia materia : listaMateria)
+            totalHoras += materia.getCargaHoraria();
+        return totalHoras;
+   } 
 }
+
+
+
